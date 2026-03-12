fe1e8bd446fa4aef89e40d29c725d84841ba6a89
Hlib-Rachkovskyy <163143242+Hlib-Rachkovskyy@users.noreply.github.com>
2026-03-12 21:45:45 +0100

    Update a trained model

--- ai_model.py
+++ ai_model.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import os
 import pickle
-from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.tree import DecisionTreeClassifier
 
 class AI:
     def __init__(self, dataset):
         self.model = None
         self.symptoms = None
         self.diseases = None
-        self.file = 'trained_knn_model.pkl'
+        self.file = 'trained_logreg_model.pkl'
         self.train_data = None
         self.evaluate_data = None
 
@@ -27,12 +26,16 @@ class AI:
 
     def learn(self, dataset):
         file_path = dataset
+        print("1. Loading dataset into memory...")
         data = pd.read_csv(file_path, encoding='utf-8')
+
+        print(f"   -> Dataset loaded: {len(data)} rows found.")
 
         data = data.sample(frac=1, random_state=42).reset_index(drop=True)
 
         split_index = int(len(data) * 0.8)
 
+        print("2. Splitting data into training and evaluation sets...")
         train_data, evaluate_data = data.iloc[:split_index], data.iloc[split_index:]
 
         self.evaluate_data = evaluate_data
@@ -43,15 +46,19 @@ class AI:
         self.symptoms = columns.columns.tolist()
         self.diseases = rows.unique()
 
-        self.model = KNeighborsClassifier(n_neighbors=5)
+        print(f"   -> Setup complete. Found {len(self.symptoms)} symptoms and {len(self.diseases)} diseases.")
+        print("3. Starting Decision Tree training...")
+
+        self.model = DecisionTreeClassifier(random_state=42)
         self.model.fit(columns, rows)
-        print("Model trained successfully...")
+
+        print("\nModel trained successfully!")
 
     def evaluate(self):
         print("Evaluating model...")
         value = self.evaluate_model()
         print("Model evaluated successfully...")
-        print("Accuracy: ${value * 100:.2f}%")
+        print(f"Accuracy: {value * 100:.2f}%")
 
     def model_predict(self, symptoms_in):
         input_data = pd.DataFrame([symptoms_in], columns=self.symptoms)
